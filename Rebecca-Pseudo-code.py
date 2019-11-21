ed63c939b23623cee83e92dffad5b7eac82f7e50
RebeccaSvensson <rebeccasvensson.1996@gmail.com>
2019-11-21 23:10:43 +0100

    Draft of pseudo code added

---
+++ Rebecca-Pseudo-code.py
@@ -0,0 +1,55 @@
+class passenger:
+
+    def __init__(self):
+        self.current_pos
+        self.seat
+        self.seated
+        self.asile
+        self.outside
+
+        self.id     # useful if sorted lists are used
+
+    def move(self, passenger):
+
+    def check_if_move_possible(self, passenger):
+        if possible:
+            return True
+        else:
+            return False
+
+class cell:
+
+    def __init__(self, pos):
+        self.pos = pos
+        self.empty = True
+        self.type       # seat/asile
+        self.status     # occupied/empty
+
+
+class plane:
+
+    def __init__(self):
+        self.grid
+
+        # Needed? If needed, use sorted lists?
+        self.waiting_passengers
+        self.seated_passengers
+        self.in_aisle_passengers
+
+        self.boarding_groups
+
+    def board_next_group(self):
+        self.in_asile_passengers.append(group)
+
+def assign_seats():
+    # randomise seats
+
+def create_boarding_groups(pattern, passengers):
+    if pattern is back_to_front:
+        return groups
+    elif pattern is random:
+        return groups
+    elif pattern is ...:
+        return groups
+
+
